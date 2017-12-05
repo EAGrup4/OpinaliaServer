7fb30524ee141f6af72006aef80fb526db41495d
guillemmunozferran <guillemeetac@gmail.com>
2017-12-05 10:45:25 +0100

    Release 0.2.0

--- routes/user.js
+++ routes/user.js
@@ -24,11 +24,13 @@ router.post('/login', users.loginUser);
 //UPDATE REQUESTS
 //Update user by id
 router.post('/:userId', md_auth.ensureAuth, users.updateUser);
+//Update provisional
 router.post('/update/:userId', users.updateUser2);
 
 //DELETE REQUESTS
 //Delete user by id
 router.delete('/:userId', md_auth.ensureAuth, users.deleteUser);
+//Delete provisional
 router.delete('/delete/:userId', users.deleteUser2);
 
 
