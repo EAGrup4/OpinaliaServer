96ffc999c429a8912e259b86a1efb4a13f60afde
guillemmunozferran <guillemeetac@gmail.com>
2018-01-04 21:01:07 +0100

    Deleted provisional update/delete functions

--- routes/user.js
+++ routes/user.js
@@ -25,14 +25,12 @@ router.post('/loginFB', users.loginUserFB);
 //UPDATE REQUESTS
 //Update user by id
 router.post('/:userId', md_auth.ensureAuth, users.updateUser);
-//Update provisional
-router.post('/update/:userId', users.updateUser2);
+
 
 //DELETE REQUESTS
 //Delete user by id
 router.delete('/:userId', md_auth.ensureAuth, users.deleteUser);
-//Delete provisional
-router.delete('/delete/:userId', users.deleteUser2);
+
 
 
 module.exports=router;
